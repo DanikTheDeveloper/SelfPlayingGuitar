09dda7e46acbf7036bc5d5c0fe21b54afba0cd8c
DanikTheDeveloper <69887442+DanikTheDeveloper@users.noreply.github.com>
2023-07-11 20:20:08 -0700

    Update selfPlayingGuitar.c

--- selfPlayingGuitar.c
+++ selfPlayingGuitar.c
@@ -3,41 +3,40 @@
 #include <pthread.h>
 #include <pruio.h>
 
-// Hypothetically, we map each GPIO to the specific servo
-#define STRUM_SERVO_GPIO 60
-#define STRING1_SERVO_GPIO 50
-#define STRING2_SERVO_GPIO 51
-#define STRING3_SERVO_GPIO 30
-#define STRING4_SERVO_GPIO 31
-#define STRING5_SERVO_GPIO 48
-#define STRING6_SERVO_GPIO 49
+#define NOTE1_GPIO 60
+#define NOTE2_GPIO 50
+#define NOTE3_GPIO 51
+#define NOTE4_GPIO 30
+#define NOTE5_GPIO 31
+#define NOTE6_GPIO 48
+#define NOTE7_GPIO 49
 
-#define STRUM_PWM_FREQ 50    // 50Hz
-#define STRUM_PWM_DUTY 2000  // 2ms
-#define STRING_PWM_FREQ 50   // 50Hz
-#define STRING_PWM_DUTY 1500 // 1.5ms
+#define NOTE1_FREQ 50   // 50Hz
+#define NOTE1_TIME 2000 // 2ms
+#define OTHER_NOTE_FREQ 50 // 50Hz
+#define OTHER_NOTE_TIME 1500 // 1.5ms
 
 #define STRUM_TIME 1000000 // 1 second
 
-#define FRET_COUNT 7
+#define NOTE_COUNT 7
 
-struct servo_gpio_mapping {
+struct note_gpio_mapping {
     uint8_t gpio_pin;
     uint32_t pwm_freq;
     uint32_t pwm_duty;
 };
 
-servo_gpio_mapping servo_mappings[FRET_COUNT] = {
-    {STRING1_SERVO_GPIO, STRING_PWM_FREQ, STRING_PWM_DUTY},
-    {STRING2_SERVO_GPIO, STRING_PWM_FREQ, STRING_PWM_DUTY},
-    {STRING3_SERVO_GPIO, STRING_PWM_FREQ, STRING_PWM_DUTY},
-    {STRING4_SERVO_GPIO, STRING_PWM_FREQ, STRING_PWM_DUTY},
-    {STRING5_SERVO_GPIO, STRING_PWM_FREQ, STRING_PWM_DUTY},
-    {STRING6_SERVO_GPIO, STRING_PWM_FREQ, STRING_PWM_DUTY},
-    {STRUM_SERVO_GPIO, STRUM_PWM_FREQ, STRUM_PWM_DUTY},
+note_gpio_mapping note_mappings[NOTE_COUNT] = {
+    {NOTE2_GPIO, OTHER_NOTE_FREQ, OTHER_NOTE_TIME},
+    {NOTE3_GPIO, OTHER_NOTE_FREQ, OTHER_NOTE_TIME},
+    {NOTE4_GPIO, OTHER_NOTE_FREQ, OTHER_NOTE_TIME},
+    {NOTE5_GPIO, OTHER_NOTE_FREQ, OTHER_NOTE_TIME},
+    {NOTE6_GPIO, OTHER_NOTE_FREQ, OTHER_NOTE_TIME},
+    {NOTE7_GPIO, OTHER_NOTE_FREQ, OTHER_NOTE_TIME},
+    {NOTE1_GPIO, NOTE1_FREQ, NOTE1_TIME},
 };
 
-void* chordFunction(void *vargp)
+void* playChord(void *vargp)
 {
     pruIo *io = pruio_new(PRUIO_DEF_ACTIVE, 0x98, 0, 1);
     if (io->Err) {
@@ -46,9 +45,9 @@ void* chordFunction(void *vargp)
         return NULL;
     }
 
-    for (int i = 0; i < FRET_COUNT - 1; i++)
+    for (int i = 0; i < NOTE_COUNT - 1; i++)
     {
-        pruio_cap_config(io, servo_mappings[i].gpio_pin, servo_mappings[i].pwm_freq);
+        pruio_cap_config(io, note_mappings[i].gpio_pin, note_mappings[i].pwm_freq);
         if (io->Err) {
             printf("failed setting CAP mode (%s)\n", io->Err);
             pruio_destroy(io);
@@ -60,7 +59,7 @@ void* chordFunction(void *vargp)
     return NULL;
 }
 
-void* strumFunction(void *vargp)
+void* playNote1(void *vargp)
 {
     pruIo *io = pruio_new(PRUIO_DEF_ACTIVE, 0x98, 0, 1);
     if (io->Err) {
@@ -69,7 +68,7 @@ void* strumFunction(void *vargp)
         return NULL;
     }
 
-    pruio_cap_config(io, servo_mappings[FRET_COUNT - 1].gpio_pin, servo_mappings[FRET_COUNT - 1].pwm_freq);
+    pruio_cap_config(io, note_mappings[NOTE_COUNT - 1].gpio_pin, note_mappings[NOTE_COUNT - 1].pwm_freq);
     if (io->Err) {
         printf("failed setting CAP mode (%s)\n", io->Err);
         pruio_destroy(io);
@@ -84,8 +83,8 @@ void* strumFunction(void *vargp)
 int main()
 {
     pthread_t thread_id1, thread_id2;
-    pthread_create(&thread_id1, NULL, chordFunction, NULL);
-    pthread_create(&thread_id2, NULL, strumFunction, NULL);
+    pthread_create(&thread_id1, NULL, playChord, NULL);
+    pthread_create(&thread_id2, NULL, playNote1, NULL);
     pthread_join(thread_id1, NULL);
     pthread_join(thread_id2, NULL);
 
